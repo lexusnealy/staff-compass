51f1b02a10789b2886c5dc6fc3030a8e30725628
lexusnealy <lexus.elisha1@gmail.com>
2025-03-15 14:35:04 -0400

    added and made changes to files

---
+++ database/seeds.sql
@@ -0,0 +1,23 @@
+-- Insert departments
+INSERT INTO department (name)
+VALUES ('Interior Design'),
+       ('IT'),
+       ('Full stack Dev'),
+       ('Design Consultant'),
+       ('Project Management');
+
+-- Insert roles
+INSERT INTO role (title, salary, department_id)
+VALUES ('Interior Designer', 80000, 1),
+       ('IT Specialist', 80000, 2),
+       ('Full stack Dev', 110000, 3),
+       ('Design Consultant', 70000, 4),
+       ('Project Manager', 100000, 5);
+
+-- Insert employees
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Destiny', 'Ray', 1, null),
+       ('Hollie', 'Sails', 2, 1),
+       ('Jacob', 'Bolawitz', 3, 1),
+       ('Larry', 'Dirt', 4, 1),
+       ('Amanda', 'Pultz', 5, null);
